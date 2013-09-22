0d04c82ab82aad1ee1b2ade0289acb043b1cbb73
carloapas <carloapas@gmail.com>
2013-09-22 10:14:27 +0800

    Replaced the domain clinicdb with Student

---
+++ grails-app/domain/eclinic/Student.groovy
@@ -0,0 +1,21 @@
+package eclinic
+
+class Student {
+
+	String firstName
+	String middleName
+	String lastName
+	String college
+	String major
+	String idNumber
+	String gender
+	Date birthday
+	String address
+	String weight
+	String height
+	String bloodType
+
+
+    static constraints = {
+    }
+}
