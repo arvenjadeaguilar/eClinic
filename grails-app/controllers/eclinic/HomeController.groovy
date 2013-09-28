90bb16bd7f253e60c7be241ae6eef5b702143f24
carloapas <carloapas@gmail.com>
2013-09-28 09:57:46 +0800

    Deleted unnecessary parts of controller

--- grails-app/controllers/eclinic/HomeController.groovy
+++ grails-app/controllers/eclinic/HomeController.groovy
@@ -7,9 +7,6 @@ class HomeController {
 
 	def dataSource
 	 
-	//def db = [url:'jdbc:postgresql://localhost:5432/eClinic', user:'postgres', password:'postgres', driver:'org.postgresql.Driver']
-	//def sql = Sql.newInstance(db.url, db.user, db.password, db.driver)
-
     def index() { }
 
 	
@@ -30,18 +27,6 @@ class HomeController {
 		render(template:"templates/graph")
 	}
 	
-	//def addStudent(){
-		//def db = new Sql(dataSource)
-	
-		
-		
-		//println "parameters are" + params;
-		//println "name: " + params.fname +" "+params.mname+" "+params.lname+""+params.sex+""+params.month;
-	
-		//def firstName = params.fname;
-		//db.execute("insert into clinicdb(fname,lname,mname,college) values('${firstName}')");
-		
-	//}
 	
 	def editPrescription() {
 		def db = new Sql(dataSource)
@@ -54,6 +39,7 @@ class HomeController {
 		render(template:"templates/profile")
 		
 	}
+	
 	
 	def diagnosisIdGenerate(){
 		Date now = new Date()
